f82cdbf43f0fd33b2464d1ce1ac5b78cfa830041
Kenric D'Souza <kenric.dsouza@gmail.com>
2017-01-22 02:58:32 +0530

    Temporarily disabled ceiling for easier lighting
    Added a load level script to start the game. Attempted using Door
    animations to open the door but we are opting to use scripts since it is
    easier and faster.

---
+++ Meltdown/Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour {
+    public string NameOfLevelToLoad;
+	// Use this for initialization
+
+    public void Load()
+    {
+        SceneManager.LoadScene(NameOfLevelToLoad);
+    }
+
+}
---
+++ Meltdown/Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour {
+    public bool isOpen = false;
+    public bool Open = false;
+    public Animator ani;
+
+	// Update is called once per frame
+	void onMouseOver()
+    {
+        if (Input.GetMouseButton(0)) {
+            ani.SetTrigger("OnDoorClick");
+        }
+    }
+}
